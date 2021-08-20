07d6b482fd9336ef077a9630866e8a0cfe94c5ec
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-08-20 18:25:16 -0500

    nav bar mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { NavBar } from "./Components/NavBar.js";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 function App() {
   return <NavBar />;
