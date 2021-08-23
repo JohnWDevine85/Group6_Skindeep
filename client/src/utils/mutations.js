90ec966d5a02c0775c942471735cd98ee7dd8cce
Molly Wilson <molly_wilson1@outlook.com>
2021-08-23 09:38:23 -0500

    create LOGIN utility function that invokes the login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,26 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $password: String!, $email: String!) {
-    addUser(username: $username, password: $password, email: $email) {
+mutation addUser($username: String!, $password: String!, $email: String!, $bio: String) {
+    addUser(username: $username, password: $password, email: $email, bio: $bio) {
         token
         user {
             _id
             username
             email
+            bio
+        }
+    }
+}
+`;
+
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
         }
     }
 }
