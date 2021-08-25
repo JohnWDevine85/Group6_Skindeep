a87bc54122f1e294a1d7225261e426de8f85ba9b
kgentel <katie.m.gentel@gmail.com>
2021-08-24 22:41:09 -0500

    adding cards to homepage and hardcoding username

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Drawer from "./components/Drawer/Drawer";
+import Homepage from "./components/Homepage/Homepage";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <ApolloProvider client={client}>
+
+      <div>
+        <NavBar />
+        <Homepage />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Drawer/Drawer.jsx
+++ client/src/components/Drawer/Drawer.jsx
@@ -1,32 +1,27 @@
+import { useQuery } from "@apollo/client";
 import React, { useState } from "react";
 import { Col, Collapse, Container, Row } from "react-bootstrap";
 import { slide as Menu } from "react-burger-menu";
-import "./Drawer.css"
+import { GET_ME_BASIC, GET_USER } from "../../utils/queries";
+import "./Drawer.css";
 
-class Drawer extends React.Component {
-  showSettings(event) {
+const Drawer = () => {
+
+  const showSettings = (event) => {
     event.preventDefault();
   }
 
-  render() {
+  const { loading, data } = useQuery(GET_USER, {
+    variables: {
+      username: "test"
+    }});
+    const user = data?.user || '';
 
-    return (
-      <Menu>
-        <a id="home" className="menu-item" href="/">
-          Home
-        </a>
-        <a id="about" className="menu-item" href="/about">
-          About
-        </a>
-        <a id="contact" className="menu-item" href="/contact">
-          Contact
-        </a>
-        <a onClick={this.showSettings} className="menu-item--small menu-item" href="">
-          Settings
-        </a>
-      </Menu>
-    );
-  }
-}
+  return (
+    <Menu showSettings={showSettings}>
+      {loading ? <div>Loading...</div> : <p id="username" className="item">{user.username} {user.email}</p>}
+    </Menu>
+  );
+};
 
 export default Drawer;
