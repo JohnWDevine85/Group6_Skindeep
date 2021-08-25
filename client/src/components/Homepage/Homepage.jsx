f102e56a192c7159543b0d6e058a2f763200c5f6
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-08-25 15:31:19 -0500

    added images

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import { Col, Collapse, Container, Row } from "react-bootstrap";
 
-const test = ["Hi this is an array", "Another one", "The last one", "Another one one", "Is this the last one?"]
+import images from "../../images";
 
 export const Homepage = () => {
   return (
     <Row xs={1} md={2} className="g-4">
-      {test.map((testItem) => (
+      {images.map((img) => (
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={img.default} />
             <Card.Body>
-              <Card.Title>{testItem}</Card.Title>
-              <Card.Text>
+              {/* <Card.Title>{}</Card.Title> */}
+              {/* <Card.Text>
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
-              </Card.Text>
+              </Card.Text> */}
             </Card.Body>
           </Card>
         </Col>
---
+++ client/src/images/index.js
@@ -0,0 +1,25 @@
+const image1 = require("./image1.jpeg");
+const image2 = require("./image2.webp");
+const image3 = require("./image3.jpeg");
+const image4 = require("./image4.jpeg");
+const image5 = require("./image5.webp");
+const image6 = require("./image6.jpeg");
+const image7 = require("./image7.jpeg");
+const image8 = require("./image8.jpeg");
+const image9 = require("./image9.jpeg");
+const image10 = require("./image10.jpeg");
+
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+];
+
+module.exports = images;
