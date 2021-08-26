e95972cae45f00033db44653c7913f5ba7364fde
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-08-25 19:00:40 -0500

    aligned photos

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -6,27 +6,29 @@ import images from "../../images";
 
 export const Homepage = () => {
   return (
-    <Row style={{ backgroundColor: "#282c34" }} xs={1} md={2} className="g-4">
-      {images.map((img) => (
-        <Col>
-          <Card
-            style={{
-              width: "40rem",
-              backgroundColor: "#282c34",
-              color: "#e52fbd",
-            }}
-          >
-            <Card.Img variant="top" src={img.img.default} />
-            <Card.Body>
-              <Card.Title>
-                {img.title}
-                <Button style={{ float: "right" }}>Like</Button>
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <Container style={{ justifyContent: "center" }}>
+      <Row style={{ backgroundColor: "#282c34" }} xs={1} md={2} className="g-4">
+        {images.map((img) => (
+          <Col>
+            <Card
+              style={{
+                width: "40rem",
+                backgroundColor: "#282c34",
+                color: "#e52fbd",
+              }}
+            >
+              <Card.Img variant="top" src={img.img.default} />
+              <Card.Body>
+                <Card.Title>
+                  {img.title}
+                  <Button style={{ float: "right" }}>Like</Button>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
