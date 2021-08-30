2b241a7b3c7810b2742c3074c53289395d449c0f
Molly Wilson <molly_wilson1@outlook.com>
2021-08-29 20:43:35 -0500

    I'm so sorry... removed drawer because we are no longer using it and it is causing errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.jsx";
-import Drawer from "./components/Drawer/Drawer.jsx";
 import Homepage from "./components/Homepage/Homepage";
 import {
   ApolloClient,
